a96fba5e13f89462a1ac5966bfa819c8bee73457
Vlad <vladdimisca@gmail.com>
2023-02-05 18:39:26 +0200

    Remove unused import

--- src/main/java/com/amss/homemanagement/model/Task.java
+++ src/main/java/com/amss/homemanagement/model/Task.java
@@ -1,7 +1,6 @@
 package com.amss.homemanagement.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
